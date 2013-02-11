4d816e4c3075be46a6731b6c9bef61fb8a6648f8
Devon Weaver <devon.weaver@gmail.com>
2013-02-10 21:44:11 -0500

    added new function to validate login information

--- MaadRMS.py
+++ MaadRMS.py
@@ -1,18 +1,43 @@
 # MaadRMS -- Open Source Restaurant Management System
 # Created by: Maadworks Software
-from flask import Flask, Session
+from flask import Flask, Session, request
 from flask.templating import render_template
 
 app = Flask(__name__)
 
+##
+# Supporting Functions
+##
+
+def valid_login(username, pass1, pass2):
+    """Validates if the username and passwords are in proper
+    formats"""
+    return False
+
+def login_the_user(username):
+    """Looks for the user in the database"""
+    pass
+
+##
+# routes
+##
 
 @app.route('/')
 def get_home():
-    return 'Welcome To MaadRMS'
+    return render_template('home_tpl.html')
 
-@app.route('/login')
+@app.route('/login', methods=['POST','GET'])
 def get_login():
-    return render_template('login_tpl.html')
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+            request.form['password1'], request.form['password2']):
+            return login_the_user(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below this is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login_tpl.html', error=error)
 
 @app.route('/kitchen')
 def get_kitchen():
@@ -37,6 +62,7 @@ def get_managers():
 @app.route('/order')
 def online_order():
     return 'Online Ordering'
+
 
 if __name__ == '__main__':
     app.run()
